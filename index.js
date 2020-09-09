5cf7504e10dd42e21e13a1d63028669b8990f16d
Shubham Singh <singh.shubham987@gmail.com>
2020-09-09 21:58:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-require('@tensorflow/tfjs-backend-cpu');
-require('@tensorflow/tfjs-backend-webgl');
-const cocoSsd = require('@tensorflow-models/coco-ssd');
-
-(async () => {
-  const img = document.getElementById('img');
-
-  // Load the model.
-  const model = await cocoSsd.load();
-
-  // Classify the image.
-  const predictions = await model.detect(img);
-
-  console.log('Predictions: ');
-  console.log(predictions);
-})();
+shubham singh
